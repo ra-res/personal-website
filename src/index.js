d0ac55aab6c94c25ba37130331e5dab9274180a8
ra-res <ra-res@users.noreply.github.com>
2021-08-07 22:16:33 +0100

    Add colour picker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './style.css'
-import $ from 'jquery'
 
 const canvas = document.getElementById("canvas1");
-$(canvas).css("background-color", "black")
+canvas.style.backgroundColor = "black";
 const ctx = canvas.getContext('2d');
 const navbar = document.getElementById("navbar");
 let navbarCoord = {
@@ -16,17 +15,41 @@ canvas.height = window.innerHeight;
 let numberOfParticles = (canvas.height * canvas.width) / 6000;
 let particlesArray;
 
+let connection = [
+    [],
+    [1, 1],
+    [10, 10],
+    [20, 5],
+    [15, 3, 3, 3],
+    [20, 3, 3, 3, 3, 3, 3, 3],
+    [12, 3, 3]
+]
+
+const themes = {
+    dark: {
+        particle: {r: 255, b:255, g:255},
+        background: "#000000"
+    },
+    light: {
+        particle: {r:0, b:0, g:0},
+        background: "#FFFFFF"
+    },
+    orange: {
+        particle: {r:0, b:0, g:0},
+        background: "#b3864d"
+    }
+}
+
 let config = {
-    backgroundColor: "black",
-    particleSize: 3.5,
-    particleColour: "white",
-    connectionSize: 7.5,
-    connectionColour: "white",
+    moving: true,
+    particleSize: 1,
+    connectionType: connection[0],
+    theme: themes.dark,
 }
 
 let particleSizeInput = document.getElementById("particleSize");
 particleSizeInput.min = 1
-particleSizeInput.max= 7
+particleSizeInput.max = 7
 particleSizeInput.value= config.particleSize
 particleSizeInput.addEventListener("input", function () {
    config.particleSize = parseInt(this.value);
@@ -34,12 +57,17 @@ particleSizeInput.addEventListener("input", function () {
 })
 
 let connectionSizeInput = document.getElementById("connectionSize");
-connectionSizeInput.min = 1;
-connectionSizeInput.max = 15;
-connectionSizeInput.value = config.connectionSize;
+connectionSizeInput.min = 0;
+connectionSizeInput.max = connection.length-1;
+connectionSizeInput.value = 0;
 connectionSizeInput.addEventListener("input", function () {
-    config.connectionSize = parseInt(this.value);
+    config.connectionType = connection[parseInt(this.value)];
     init();
+})
+
+let movement = document.getElementById("movement");
+movement.addEventListener("input", function() {
+    config.moving = movement.checked;
 })
 
 let mouse = {
@@ -54,6 +82,30 @@ window.addEventListener("mousemove",
         mouse.y = event.y;
     })
 
+document.getElementById("preset-1").addEventListener("click", function() {
+    setTheme("dark");
+});
+document.getElementById("preset-2").addEventListener("click", function() {
+    setTheme("light");
+});
+document.getElementById("preset-3").addEventListener("click", function() {
+    setTheme("orange");
+});
+document.getElementById("preset-4").addEventListener("click", function() {
+    setTheme("dark");
+});
+document.getElementById("preset-5").addEventListener("click", function() {
+    setTheme("dark");
+});
+document.getElementById("preset-6").addEventListener("click", function() {
+    setTheme("dark");
+});
+
+function setTheme(id) {
+    config.theme = themes[id]
+    canvas.style.backgroundColor = config.theme.background;
+}
+
 // particle
 class Particle {
     constructor(x, y, directionX, directionY, size, colour) {
@@ -63,12 +115,13 @@ class Particle {
         this.directionY = directionY;
         this.size = size;
         this.colour = colour;
+        this.opacity = Math.random();
     }
     //draw method
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle = "#ffffff";
+        ctx.fillStyle = this.colour;
         ctx.fill();
     }
 
@@ -114,24 +167,25 @@ class Particle {
         // move particles  and draw
         this.x += this.directionX;
         this.y += this.directionY;
+        this.colour = getConnectionColour(config.theme.particle, this.opacity);
         this.draw();
     }
 }
 
-function scale (number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-}
-
 function createParticleAttributes() {
-    let size, x, y, directionX, directionY, colour = "#ffffff";
+    let size,
+        x,
+        y,
+        directionX,
+        directionY,
+        colour = getConnectionColour(config.theme.particle, Math.random);
     while (true) {
-
         size = (Math.random() * config.particleSize) + 1;
         x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2)
         y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2)
         directionX = Math.random() - 0.5;
         directionY = Math.random() - 0.5;
-        colour = "rgba(255,255,255," + Math.random() + ")";
+
         let p = {
             size: size,
             x: x,
@@ -140,12 +194,12 @@ function createParticleAttributes() {
             directionY: directionY,
             colour: colour
         }
+
         if (!isInsideNavbar(p)) {
             return p;
         }
     }
 }
-
 
 function init() {
     particlesArray = []
@@ -179,10 +233,16 @@ function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
 
-    for (let i = 0; i < particlesArray.length; i++) {
-        particlesArray[i].update();
+    if (config.moving){
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+        }
     }
     connect();
+}
+
+function getConnectionColour(rgb, opacity) {
+    return "rgba(" + rgb.r + "," + rgb.g + "," + rgb.b + "," + opacity + ")";
 }
 
 function connect() {
@@ -190,14 +250,11 @@ function connect() {
     for (let a = 0; a < particlesArray.length; a++) {
         for (let b = a; b < particlesArray.length; b++) {
             let distance = Math.pow(particlesArray[a].x - particlesArray[b].x, 2) + Math.pow(particlesArray[a].y - particlesArray[b].y, 2);
-            // let distance = ((particlesArray[a].x - particlesArray[b].x)
-            // * (particlesArray[a].x - particlesArray[b].x))
-            // + ((particlesArray[a].y - particlesArray[b].y)
-            // * (particlesArray[a].y - particlesArray[b].y));
             if (distance < (canvas.width / 7) * (canvas.height / 7))  {
                 opacityValue = 1 - (distance / 20000);
-                ctx.strokeStyle = "rgba(255,255,255," + opacityValue + ")";
+                ctx.strokeStyle = getConnectionColour(config.theme.particle, opacityValue);
                 ctx.beginPath();
+                ctx.setLineDash(config.connectionType);
                 ctx.moveTo(particlesArray[a].x, particlesArray[a].y)
                 ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
                 ctx.stroke()
